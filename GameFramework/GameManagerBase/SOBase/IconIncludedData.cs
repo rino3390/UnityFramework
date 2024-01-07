868359dfa6a2391d71e3aa3cb4808e9a76e7c04c
Rino <sherikawaii3390@gmail.com>
2024-01-07 16:34:17 +0800

    make icon not required for icon included data

--- GameFramework/GameManagerBase/SOBase/IconIncludedData.cs
+++ GameFramework/GameManagerBase/SOBase/IconIncludedData.cs
@@ -6,14 +6,9 @@ namespace GameFramework.GameManagerBase.SOBase
 {
 	public abstract class IconIncludedData: InfoData
 	{
-		[HideLabel, PreviewField(70,ObjectFieldAlignment.Center)]
-		[HorizontalGroup(LayoutConst.TopInfoLayout,200)]
-		[PropertyOrder(-1),PropertySpace(20)]
+		[HideLabel, PreviewField(70, ObjectFieldAlignment.Center)]
+		[HorizontalGroup(LayoutConst.TopInfoLayout, 200)]
+		[PropertyOrder(-1), PropertySpace(20)]
 		public Sprite Icon;
-		
-		public override bool IsDataLegal()
-		{
-			return Icon && base.IsDataLegal();
-		}
 	}
 }
