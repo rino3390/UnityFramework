87f8410ba2269bf2952cf1ba609fec433339e89e
Rino <sherikawaii3390@gmail.com>
2024-10-31 19:39:28 +0800

    add localize text in menu item

---
+++ GameFramework/RinoUtility/Editor/UnitySceneObject/CreateLocalizeObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TMPro;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+using UnityEngine.SceneManagement;
+
+namespace GameFramework.RinoUtility.Editor.UnitySceneObject
+{
+	public class CreateLocalizeObject
+	{
+		[MenuItem("GameObject/UI/Localize/Text", false, 0)]
+		public static void CreateLocalizeText(MenuCommand menuCommand)
+		{
+			CreateLocalizeObj("LocalizeText", typeof(TextMeshProUGUI), typeof(LocalizeStringEvent));
+		}
+
+		public static void CreateLocalizeObj(string name, params Type[] types)
+		{
+			var newObj = ObjectFactory.CreateGameObject(name, types);
+			Place(newObj);
+		}
+
+		public static void Place(GameObject gameObject)
+		{
+			gameObject.transform.position = Vector3.zero;
+			StageUtility.PlaceGameObjectInCurrentStage(gameObject);
+			GameObjectUtility.EnsureUniqueNameForSibling(gameObject);
+			Undo.RegisterCreatedObjectUndo(gameObject, "Create " + gameObject.name);
+			Selection.activeObject = gameObject;
+			EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+		}
+	}
+}
